a7bf75fbfebb548dbdb0af836b9cea6b578ef90c
misheck12 <livingim2018@gmail.com>
2023-06-09 16:30:33 +0000

    Jun 9, 2023, 4:30 PM

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_122900) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_134205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
